f6812217157cc63564c3d28cc5d1cd29aa29630e
redfox-mx <g11ada15@hackademy.mx>
2020-12-21 21:39:20 -0600

    update: create elements by firestore

--- js/app.js
+++ js/app.js
@@ -1,38 +1,35 @@
 const settings = {
     input: {
-        class: ['form-check-input', 'me-1'],
+        styles: ['form-check-input', 'me-1'],
         type: 'checkbox'
     },
     list: {
-        class: 'list-group-item'
+        styles: ['list-group-item']
     }
 }
 
-function addTask(title){
-    let list = document.getElementById('task-list');
+let list = document.getElementById('task-list');
+
+function addTask(task, id) {
     let listItem = document.createElement('li');
     let input = document.createElement('input');
 
-    input.setAttribute( 'type', settings.input.type);
-    settings.input.class.forEach(_ => input.classList.add(_));
-    
-    
+    settings.input.styles.forEach( style => input.classList.add(style) );
+    settings.list.styles.forEach( style => listItem.classList.add(style) );
+
+    input.setAttribute('type', settings.input.type);
+    listItem.setAttribute( 'data-id', id);
     listItem.appendChild(input);
-    listItem.innerHTML += title;
-    listItem.classList.add(settings.list.class);
+    listItem.innerHTML += task.title;
+
+    if(task.done) listItem.classList.add('done');
 
     list.appendChild(listItem);
 }
 
-
-/*
-let listElement = document.querySelectorAll(".form-check-input");
-for(item of listElement){
-    item.addEventListener('click', (e)=>{
-        e.target.parentElement.classList.toggle('done')
-    })
-}
-*/
+db.collection('todo').get().then((snapshot)=>{
+    snapshot.docs.forEach( doc => addTask(doc.data(), doc.id) )
+})
 
 
 
